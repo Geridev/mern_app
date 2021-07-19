3e97bcccaa4e3de4c30084c90b65d20daa9688f1
Geridev <lovasgergo44@gmail.com>
2021-07-19 22:50:51 +0200

    router létrehozása

--- server/index.js
+++ server/index.js
@@ -1,2 +1,22 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import postsRouter from "./routes/posts.js";
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+app.use("/posts", postsRouter);
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => app.listen(port, () => console.log(`app listening to ${port}`)))
+  .catch((error) => console.log(error.message));
